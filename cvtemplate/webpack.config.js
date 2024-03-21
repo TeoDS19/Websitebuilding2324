18f942944ee54a4b93a5725673caa21ef94dbe15
Frontend JavaScript <frontendjavascript@iMac-04.local>
2024-03-21 20:49:24 +0200

    curs4

--- cvtemplate/webpack.config.js
+++ cvtemplate/webpack.config.js
@@ -17,6 +17,7 @@ const myHeader = fs.readFileSync(paths.source + '/views/header.html');
 const myBanner = fs.readFileSync(paths.source + '/views/banner.html');
 const myAbout = fs.readFileSync(paths.source + '/views/about.html');
 const myExperience=fs.readFileSync(paths.source+'/views/experience.html');
+const myPortfolio=fs.readFileSync(paths.source+'/views/portfolio.html');
 const myContact = fs.readFileSync(paths.source + '/views/contact.html');
 const myFooter = fs.readFileSync(paths.source + '/views/footer.html');
 module.exports = {
@@ -39,6 +40,7 @@ module.exports = {
             myBanner: myBanner,
             myAbout: myAbout,
             myExperience:myExperience,
+            myPortfolio:myPortfolio,
             myContact: myContact,
             myFooter: myFooter,
             template: './src/index.html',
